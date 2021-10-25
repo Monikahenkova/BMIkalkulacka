110b68af5b83549c718b0db67e0dc436f599484a
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2021-10-25 21:09:20 +0200

    Přidejte soubory projektu.

---
+++ BMI2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Výpočet BMI");
+         Console.WriteLine("****************");
+         Console.WriteLine("Zadej váhu v kg:");
+         float váha = float.Parse(Console.ReadLine());
+         Console.WriteLine("Zadej výšku m:");
+         float výška = float.Parse(Console.ReadLine());
+         float BMI = váha / (výška * výška);
+         Console.WriteLine("Vypočítané BMI: " + BMI);
+         Console.WriteLine(" Děkuji za použití BMI kalkulačky ");
+         Console.ReadKey();
+        }
+            
+    }
+}
+
